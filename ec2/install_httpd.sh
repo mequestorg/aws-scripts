07c329d0ab8947695e43a6296cf8512cb3c4355d
Itao-o-bong✨ <45539206+itaobong@users.noreply.github.com>
2024-04-07 13:58:45 +0100

    Update install_httpd.sh

--- ec2/install_httpd.sh
+++ ec2/install_httpd.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Use this for your user data (script from top to bottom)
 # install httpd (Linux 2023 AMI version)
-sudo yum update - y 
+
+# Update the package repositories
+sudo yum update -y
+
+# Install Apache HTTP Server (httpd)
 sudo yum install -y httpd
+
+# Start and enable httpd service
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo sudo sh -c 'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html'
+
+# Create index.html file with some content
+sudo sh -c 'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html'
