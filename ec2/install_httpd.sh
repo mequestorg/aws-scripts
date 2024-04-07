44e1f897600e89d143fe201f036356c48d895a89
Itao-o-bong✨ <45539206+itaobong@users.noreply.github.com>
2024-04-07 12:53:49 +0100

    Update install_httpd.sh

--- ec2/install_httpd.sh
+++ ec2/install_httpd.sh
@@ -3,6 +3,6 @@
 # install httpd (Linux 2 version)
 yum update - y 
 yum install -y httpd
-systemct! start httpd
-systemct! enable httpd
+systemctl start httpd
+systemctl enable httpd
 echo "<h1>Hello World from $ (hostname -f)</h1>" > /var/www/html/index.html
